e65e9cbea8a4b541f34c9984cfa5502df9e4d192
김서현 <138189206+westh07@users.noreply.github.com>
2025-05-09 11:30:50 +0900

    05.09 수업 code

--- back/JAVA/12_stream/lecture/terminal/Application6_Collect.java
+++ back/JAVA/12_stream/lecture/terminal/Application6_Collect.java
@@ -8,7 +8,7 @@ public class Application6_Collect {
     public static void main(String[] args) {
       /*
       * collect()
-      * - collector 타입을 받아서 처리를 하는데, 메소드를 통해 컬렉션을 출력으로 받으ㅡㄹ 수 있다.
+      * - collector 타입을 받아서 처리를 하는데, 메소드를 통해 컬렉션을 출력으로 받을 수 있다.
       * - collector 객체에서 제공하는 정적메소드를 사용 가능
       * */
 
